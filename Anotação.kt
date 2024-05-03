861b73af8d97d6a710b35f5742429c9e053ac067
Guibr <165696239+GuiilhermeBr@users.noreply.github.com>
2024-05-03 09:01:38 -0300

    Anotações
    Anotações básica da 1ª aula

---
+++ Anotação.kt
@@ -0,0 +1,48 @@
+fun main() {
+    // Anotações em uma única linha
+
+    /* Anotação
+     * em
+     * várias
+     * linhas
+    */ 
+    
+    /* A programação é feita de variáveis e constantes
+     * Variável: Valor que pode sofrer alterações
+     * Constante: O valor vai permanecer sempre o mesmo
+     * 
+     * Tipos de variáveis
+     * 
+     * String: Variável de texto, declarada entre aspas ""
+     * Boolean: Verdadeiro ou falso (True or False)
+     * 
+     * Tipos de variáveis numéricas
+     * 
+     * Int: Guarda números inteiros (ex: 1)
+     * Float: Guarda números decimais até 7 casas, para especificar uma float é
+     * necessário colocar um f na frente (ex: 1.0000000f)
+     * Double: Guarda números decimais até 15 casas (ex: 1.000000000000000)
+     * Long: Guarda números grandes, a partir de 17 dígitos (ex: 10000000000000000)
+     */
+    
+    // Exemplo de variáveis
+    var dinheiro = 100
+    var mensagem = "Kleberson Gay"
+    
+    // Exemplo de constantes
+    val id = 1234
+    val nome = "Kleberson Reginaldo Ferreira de Oliveira"
+    
+    // String
+    var string = "string"
+    
+    // Boolean
+    var boolean = true
+    var boolean2 = false
+    
+    // Int
+    var int = 1
+    
+    // Float
+    var float = 1.1f
+}
